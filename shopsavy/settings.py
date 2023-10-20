e30506f3ba0e4d6059db9543ffc469b3a80f95e4
Umer-Ishtiaq-4 <umerishtiaq4@gmail.com>
2023-10-20 08:59:12 +0500

    setting file updated

--- shopsavy/settings.py
+++ shopsavy/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '47d)n05#ei0rg4#)*@fuhc%$5+0n(t%jgxg$)!1pkegsi*l4c%'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -196,8 +196,8 @@ MESSAGE_TAGS = {
 
 # SMTP configuration
 # SMTP configuration
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mastermalikking@gmail.com'
+EMAIL_HOST_PASSWORD = 'hotq mepi kpjb fxne'
+EMAIL_USE_TLS = True
